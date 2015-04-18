fc9b731e0a4d5e7f71254fa409b7df858db8e50e
Dhim <gamedhim@gmail.com>
2015-04-18 09:15:28 +0200

    I love the smell of the first commit in the morning

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.get('/', function(request, response) {
   response.render('pages/landing.ejs', {})
 });
 
+app.get('/game', function(request, response) {
+  response.render('pages/game.ejs', {})
+});
+
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
---
+++ public/js/game.js
@@ -0,0 +1,26 @@
+//game.js
+
+
+//Core function to be call on init 
+function GameStart () {
+
+	//load Graphical Stuff
+	webGlInit() ; 
+	//load Sounds stuff 
+	soundInit() ;
+
+}
+
+
+function webGlInit() {
+
+
+
+} 
+
+
+function soundInit() {
+
+	
+	
+}
